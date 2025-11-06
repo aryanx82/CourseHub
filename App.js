57cbe73a1a284253c4cc8f012120d22f4146e696
Aryan Yadav <aryan.yadav2024@nst.rishihood.edu.in>
2025-11-06 16:12:50 +0530

    Home Page

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/home';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Categories from './pages/categories';
+// import Login from './pages/login';
+// import SignUp from './pages/signup';
 
 export default function App() {
   return (
+    <SafeAreaProvider>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
+    </SafeAreaProvider>
   );
 }
 
